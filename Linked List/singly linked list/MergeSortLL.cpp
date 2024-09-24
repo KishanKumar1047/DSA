19c378fe6c666c69227932f20a8dab6c48df8452
= <=>
2024-09-24 22:56:15 +0530

    modify

--- Linked List/singly linked list/MergeSortLL.cpp
+++ Linked List/singly linked list/MergeSortLL.cpp
@@ -1,4 +1,3 @@
-// implementation of linked list
 #include <iostream>
 using namespace std;
 
@@ -7,99 +6,160 @@ class Node
 public:
     int val;
     Node *next;
-    Node *prev;
 
     Node(int val)
     {
         this->val = val;
         this->next = NULL;
-        this->prev = NULL;
     }
 };
 
-
-class DDL
+class LinkedList
 { // user defined data structure
 public:
     Node *head;
     Node *tail;
     int size;
 
-    DDL()
+    LinkedList()
     {
         head = tail = NULL;
         size = 0;
     }
 
+    // Merges two sorted linked lists
+    Node *merge(Node *left, Node *right)
+    {
+        if (left == NULL)
+            return right;
+        else if (right == NULL)
+            return left;
+
+        Node *result = nullptr;
+
+        if (left->val < right->val)
+        {
+            result = left;
+            result->next = merge(left->next, right);  // Fix: link the next pointer
+        }
+        else
+        {
+            result = right;
+            result->next = merge(left, right->next);  // Fix: link the next pointer
+        }
+
+        return result;
+    }
+
+    // Finds the middle of the linked list
+    Node *findMiddle(Node *head)
+    {
+        if (head == nullptr)
+            return head;
+        Node *slow = head;
+        Node *fast = head;
+
+        while (fast != NULL && fast->next != NULL)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    // Merge sort for linked list
+    Node *mergeSort(Node *node)
+    {
+        if (node == nullptr || node->next == nullptr)
+        {
+            return node;
+        }
+        Node *middle = findMiddle(node);
+        Node *nextOfMiddle = middle->next;
+        middle->next = NULL;
+
+        Node *left = mergeSort(node);
+        Node *right = mergeSort(nextOfMiddle);
+
+        return merge(left, right);
+    }
+
+    // Calls merge sort on the linked list
+    void sort()
+    {
+        head = mergeSort(head);
+    }
+
+    // Inserts a node at the end of the linked list
     void InsertAtTail(int val)
-    { // insert at end
+    {
         Node *temp = new Node(val);
         if (size == 0)
             head = tail = temp;
         else
         {
             tail->next = temp;
-            temp->prev = tail;
             tail = temp;
         }
         size++;
     }
 
+    // Inserts a node at the beginning of the linked list
     void InsertAtHead(int val)
-    { // insert at beg
+    {
         Node *temp = new Node(val);
         if (size == 0)
             head = tail = temp;
         else
         {
             temp->next = head;
-            head->prev = temp;
             head = temp;
         }
         size++;
     }
 
+    // Deletes the head node
     void DeleteAtHead()
     {
         if (size == 0)
         {
             cout << "List is Empty" << endl;
         }
-        else if(size ==1){
+        else if (size == 1)
+        {
             head = tail = NULL;
             size--;
         }
         else
-        {   
+        {
             head = head->next;
-            head->prev = NULL;
             size--;
         }
     }
+
+    // Deletes the tail node
     void DeleteAtTail()
     {
+        Node *temp = head;
         if (size == 0)
         {
             cout << "List is Empty" << endl;
-            return;
         }
-        else if(size ==1){
-            head = tail = NULL;
-            size--;
+        while (temp->next != tail)
+        {
+            temp = temp->next;
         }
-       else {
-         Node *temp = tail->prev;
         temp->next = NULL;
         tail = temp;
         size--;
-       }
     }
 
+    // Inserts a node at the given index
     void InsertAtIdx(int idx, int val)
     {
         if (idx < 0 || idx > size)
         {
-            cout << "invalid position" << endl;
+            cout << "Invalid position" << endl;
         }
         else if (idx == 0)
         {
@@ -117,14 +177,13 @@ public:
             {
                 temp = temp->next;
             }
-            temp->next->prev = t;
             t->next = temp->next;
-            t->prev = temp;
             temp->next = t;
             size++;
         }
     }
 
+    // Deletes the node at the given index
     void DeleteAtIdx(int idx)
     {
         if (size == 0)
@@ -133,13 +192,13 @@ public:
         }
         else if (idx < 0 || idx >= size)
         {
-            cout << "invalid Index" << endl;
+            cout << "Invalid Index" << endl;
         }
         else if (idx == 0)
         {
             DeleteAtHead();
         }
-        else if (idx == size-1)
+        else if (idx == size - 1)
         {
             DeleteAtTail();
         }
@@ -151,55 +210,51 @@ public:
                 temp = temp->next;
             }
             temp->next = temp->next->next;
-            temp->next->prev = temp;
             size--;
         }
     }
-    void DeleteIem(int item)
-    {
-        if (size == 0)
-        {
-            cout << "List is Empty" << endl;
+
+    void Deleteitem(int item){
+        if(head==NULL){
+            cout<<"List is empty";
+            return;
         }
-       if(head->val == item){
-        head = head->next;
-        head->next->prev = NULL;
-        size--;
-       }
-        else
-        {
-            Node *temp = head;
-            while(temp->next !=NULL && temp->next->val != item ){
+        if(head->val == item){
+            head = head->next;
+            size--;
+        }
+        else {
+            Node* temp = head;
+            while(  temp->next != NULL &&  temp->next->val !=item  ){
                 temp = temp->next;
             }
             if(temp->next == NULL){
-                cout<<"ite not found"<<endl;
-                return ;
+                cout<<"item not found"<<endl;
             }
             else {
                 temp->next = temp->next->next;
-                temp->next->prev = temp;
                 size--;
-        }   }
+            }
+        }
     }
 
+    // Gets the value of the node at the given index
     void getIdx(int idx)
     {
-        if (idx < 0 || idx >= size)
+        if (idx < 0 || idx > size)
         {
-            cout << "invalid idx" << endl;
+            cout << "Invalid idx" << endl;
         }
         else if (idx == 0)
         {
             cout << head->val << endl;
         }
-        else if (idx == size-1)
+        else if (idx == size - 1)
         {
             cout << tail->val << endl;
         }
         else
-        {if(idx<size/2){
-            
+        {
             Node *temp = head;
             for (int i = 1; i <= idx; i++)
             {
@@ -207,18 +262,9 @@ public:
             }
             cout << temp->val << endl;
         }
-        else {
-            
-            Node *temp = tail;
-            for (int i = 1; i <size - idx; i++)
-            {
-                temp = temp->prev;
-            }
-            cout << temp->val << endl;
-        }
-        }
     }
 
+    // Displays the linked list
     void display()
     {
         Node *temp = head;
@@ -231,30 +277,22 @@ public:
     }
 };
 
-int main(){
-    DDL dl;
-    dl.InsertAtTail(78);
-    dl.InsertAtTail(455);
-    dl.InsertAtTail(45);
-    dl.InsertAtHead(45);
- //   dl.display(); // 45 78 455 45
-    dl.InsertAtIdx(2,99);
-    dl.InsertAtIdx(4,98);
-    dl.display(); // 45 78  99 455 98 45
-    // cout<<dl.size<<endl ; // 6
-    // dl.DeleteAtHead();// 78 99 455 98 45
-    // dl.DeleteAtTail();// 78 99 455 98 
-    // dl.DeleteAtIdx(2);// 78 99 98
-    // dl.display(); 
-    // dl.getIdx(3);  // 455
-    dl.DeleteIem(78);
-    dl.display();
-    dl.DeleteIem(93);
-    dl.display();
-
-
-
+int main()
+{
+    LinkedList ll;
+    ll.InsertAtTail(10);   // {10}
+    ll.InsertAtTail(20);   // {10,20}
+    ll.InsertAtIdx(1, 90); // {10,90,20}
+    ll.InsertAtHead(89);   // {89,10,90,20}
+    ll.InsertAtHead(345);  // {345,89,10,90,20}
+    ll.InsertAtIdx(3, 55); // {345,89,10,55,90,20}
+    ll.display();
+    ll.Deleteitem(55);
+    ll.display();
+    ll.Deleteitem(98);
+    ll.display();
+    ll.Deleteitem(20);
+    ll.display();
 
     return 0;
-
 }
